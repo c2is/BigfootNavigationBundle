b4ce3002a306e126aabb02b8e0944e208324c360
Guillaume Manen <g.manen@c2is.fr>
2014-02-21 12:08:41 +0100

    Menu access restricted to admin

--- Listener/MenuListener.php
+++ Listener/MenuListener.php
@@ -6,12 +6,26 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 use Bigfoot\Bundle\CoreBundle\Event\MenuEvent;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Menu Listener
  */
 class MenuListener implements EventSubscriberInterface
 {
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    private $security;
+
+    /**
+     * @param SecurityContextInterface $security
+     */
+    public function __construct(SecurityContextInterface $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * Get subscribed events
      *
@@ -32,73 +46,75 @@ class MenuListener implements EventSubscriberInterface
         $menu          = $event->getSubject();
         $structureMenu = $menu->getChild('structure');
 
-        $navigationMenu = $structureMenu->addChild(
-            'navigation_menu',
-            array(
-                'label'          => 'Navigation',
-                'url'            => '#',
-                'linkAttributes' => array(
-                    'class' => 'dropdown-toggle',
-                    'icon'  => 'sitemap',
-                )
-            )
-        );
-
-        $navigationMenu->setChildrenAttributes(
-            array(
-                'class' => 'submenu',
-            )
-        );
-
-        $navigationMenu->addChild(
-            'menu',
-            array(
-                'label'  => 'Menu',
-                'route'  => 'admin_menu',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_menu_new',
-                        'admin_menu_edit'
+        if ($this->security->isGranted('ROLE_MENU')) {
+            $navigationMenu = $structureMenu->addChild(
+                'navigation_menu',
+                array(
+                    'label'          => 'Navigation',
+                    'url'            => '#',
+                    'linkAttributes' => array(
+                        'class' => 'dropdown-toggle',
+                        'icon'  => 'sitemap',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon' => 'double-angle-right',
                 )
-            )
-        );
+            );
 
-        $navigationMenu->addChild(
-            'menu_item',
-            array(
-                'label'  => 'Item',
-                'route'  => 'admin_menu_item',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_menu_item_new',
-                        'admin_menu_item_edit'
-                    )
-                ),
-                'linkAttributes' => array(
-                    'icon' => 'double-angle-right',
+            $navigationMenu->setChildrenAttributes(
+                array(
+                    'class' => 'submenu',
                 )
-            )
-        );
+            );
 
-        $navigationMenu->addChild(
-            'menu_item_attribute',
-            array(
-                'label'  => 'Attribute',
-                'route'  => 'admin_menu_item_attribute',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_menu_item_attribute_new',
-                        'admin_menu_item_attribute_edit'
+            $navigationMenu->addChild(
+                'menu',
+                array(
+                    'label'  => 'Menu',
+                    'route'  => 'admin_menu',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_menu_new',
+                            'admin_menu_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon' => 'double-angle-right',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon' => 'double-angle-right',
                 )
-            )
-        );
+            );
+
+            $navigationMenu->addChild(
+                'menu_item',
+                array(
+                    'label'  => 'Item',
+                    'route'  => 'admin_menu_item',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_menu_item_new',
+                            'admin_menu_item_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon' => 'double-angle-right',
+                    )
+                )
+            );
+
+            $navigationMenu->addChild(
+                'menu_item_attribute',
+                array(
+                    'label'  => 'Attribute',
+                    'route'  => 'admin_menu_item_attribute',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_menu_item_attribute_new',
+                            'admin_menu_item_attribute_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon' => 'double-angle-right',
+                    )
+                )
+            );
+        }
     }
 }
