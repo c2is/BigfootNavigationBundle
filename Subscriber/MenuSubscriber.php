1586b44342a9b88b97b7bcc59d53ba278b7d8bd9
Guillaume Manen <g.manen@c2is.fr>
2014-03-22 18:53:36 +0100

    menu rights

--- Subscriber/MenuSubscriber.php
+++ Subscriber/MenuSubscriber.php
@@ -46,59 +46,60 @@ class MenuSubscriber implements EventSubscriberInterface
         $menu          = $event->getSubject();
         $structureMenu = $menu->getChild('structure');
 
+        $navigationMenu = $structureMenu->addChild(
+            'navigation_menu',
+            array(
+                'label'          => 'Navigation',
+                'url'            => '#',
+                'linkAttributes' => array(
+                    'class' => 'dropdown-toggle',
+                    'icon'  => 'sitemap',
+                )
+            )
+        );
+
+        $navigationMenu->setChildrenAttributes(
+            array(
+                'class' => 'submenu',
+            )
+        );
+
+        $navigationMenu->addChild(
+            'menu',
+            array(
+                'label'  => 'Menu',
+                'route'  => 'admin_menu',
+                'extras' => array(
+                    'routes' => array(
+                        'admin_menu_new',
+                        'admin_menu_edit'
+                    )
+                ),
+                'linkAttributes' => array(
+                    'icon' => 'double-angle-right',
+                )
+            )
+        );
+
+        $navigationMenu->addChild(
+            'menu_item',
+            array(
+                'label'  => 'Item',
+                'route'  => 'admin_menu_item',
+                'extras' => array(
+                    'routes' => array(
+                        'admin_menu_item_new',
+                        'admin_menu_item_edit'
+                    )
+                ),
+                'linkAttributes' => array(
+                    'icon' => 'double-angle-right',
+                )
+            )
+        );
+
+
         if ($this->security->isGranted('ROLE_ADMIN')) {
-            $navigationMenu = $structureMenu->addChild(
-                'navigation_menu',
-                array(
-                    'label'          => 'Navigation',
-                    'url'            => '#',
-                    'linkAttributes' => array(
-                        'class' => 'dropdown-toggle',
-                        'icon'  => 'sitemap',
-                    )
-                )
-            );
-
-            $navigationMenu->setChildrenAttributes(
-                array(
-                    'class' => 'submenu',
-                )
-            );
-
-            $navigationMenu->addChild(
-                'menu',
-                array(
-                    'label'  => 'Menu',
-                    'route'  => 'admin_menu',
-                    'extras' => array(
-                        'routes' => array(
-                            'admin_menu_new',
-                            'admin_menu_edit'
-                        )
-                    ),
-                    'linkAttributes' => array(
-                        'icon' => 'double-angle-right',
-                    )
-                )
-            );
-
-            $navigationMenu->addChild(
-                'menu_item',
-                array(
-                    'label'  => 'Item',
-                    'route'  => 'admin_menu_item',
-                    'extras' => array(
-                        'routes' => array(
-                            'admin_menu_item_new',
-                            'admin_menu_item_edit'
-                        )
-                    ),
-                    'linkAttributes' => array(
-                        'icon' => 'double-angle-right',
-                    )
-                )
-            );
-
             $navigationMenu->addChild(
                 'menu_item_attribute',
                 array(
